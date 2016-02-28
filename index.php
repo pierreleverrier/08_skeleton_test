32dd0cd225e28ebfdf757026a7baac9192b983ff
pierreleverrier <leverrier.pierre@gmail.com>
2016-02-28 10:46:32 +0100

    Test Skeleton Portfolio

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Skeleton: Responsive CSS Boilerplate</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href='//fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css'>
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/custom.css">
+  <link rel="stylesheet" href="css/style5.css">
+
+  <!-- Scripts
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+</head>
+<body>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+  <div class="section home">
+
+    <nav id="bt-menu" class="bt-menu">
+    <div class="black-block">
+      <a href="index.php">
+          <img src="img/logo.png" alt="Logo" />
+      </a>
+    </div>
+    <a href="#" class="bt-menu-trigger"><span>MENU</span></a>
+      <ul>
+        <li><a href="#"><i class="fa fa-angle-right"></i> Anecgones</a></li>
+        <li><a href="#"><i class="fa fa-angle-right"></i> TV App</a></li>
+        <li><a href="#"><i class="fa fa-angle-right"></i> L'Exception</a></li>
+        <li><a href="#"><i class="fa fa-angle-right"></i> Creative Breakfast</a></li>
+        <li><a href="#"><i class="fa fa-angle-right"></i> Lyon BD</a></li>
+        <li><a href="#"><i class="fa fa-angle-right"></i> INSA</a></li>
+        <li><a href="#"><i class="fa fa-info-circle"></i> About me</a></li>
+      </ul>
+    </nav>
+
+  </div>
+
+
+
+
+
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <script src="js/classie.js"></script>
+  <script src="js/borderMenu.js"></script>
+</body>
+</html>
